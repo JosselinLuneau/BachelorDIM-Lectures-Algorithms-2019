55dd344ef95697c4e29cad540f033c60241de314
Joss <josselinluneau@gmail.com>
2019-09-26 15:30:43 +0200

    Session 1 - Exercice 1 : Average

--- S1_aglotools.py
+++ S1_aglotools.py
@@ -1,4 +1,56 @@
-print("Hello world")
+'''@package docstring
+Documentation Exercice 1 : Average.
+ 
+Learn algorithmie.
+Use Dyoxigen docs.
+Use git and versionning.
+'''
 
-myVariable=0
-print('myVariable=', myVariable)
+'''
+What happens if Som initialization is forgotten ?
+    An NameError occured and said that som is not defined.
+
+What can you expect if all the values are below zero ?
+    The averrage will be a division by 0 then it will display an error.
+
+'''
+array=[1,5,8,7,0,-5,4-2,0,9,10,7]
+
+import numpy as np
+
+tab_zeros=np.zeros(12, dtype=np.int32)
+tab_fromList=np.array(array)
+
+def average_above_zero(array):
+    '''
+    Function that return average from array
+    Args:
+        array: an array
+    Return the average of positive number above zero
+    '''
+
+    som=0
+    n=0
+    for i in range(len(array)):
+        if array[i] > 0:
+            som += int(array[i])
+            n += 1
+
+    try:
+        average=som/n
+
+        return average
+    except Exception as e:
+ 
+        return e.args
+
+
+average_one = average_above_zero(array)
+print("The fisrt average is {0}".format(average_one))
+
+average_two = average_above_zero(tab_zeros)
+print("The second average is {0} (An error)".format(average_two))
+
+average_three = average_above_zero(tab_fromList)
+print("The third average is {0} (same as first)".format(average_three))
+
