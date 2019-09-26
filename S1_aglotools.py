c5bec41a66a09b6ebdc3e7a3238920a0eb8f1e18
Joss <josselinluneau@gmail.com>
2019-09-26 16:15:10 +0200

    Session 1 - Exercice 2 : Table maximun value

--- S1_aglotools.py
+++ S1_aglotools.py
@@ -14,7 +14,9 @@ What can you expect if all the values are below zero ?
     The averrage will be a division by 0 then it will display an error.
 
 '''
-array=[1,5,8,7,0,-5,4-2,0,9,10,7]
+
+''' Exercice 1 : Average '''
+array=[1,5,8,7,0,-5,4-2,0,10,9,7]
 
 import numpy as np
 
@@ -25,6 +27,7 @@ def average_above_zero(array):
     '''Function that return average from array
     Arg:
         @param array: an array
+
     Return the average of positive number above zero
     '''
 
@@ -53,3 +56,26 @@ print("The second average is {0} (An error)".format(average_two))
 average_three = average_above_zero(tab_fromList)
 print("The third average is {0} (same as first)".format(average_three))
 
+''' Exercice 2 : Table maximum value '''
+
+def max_value(array):
+    '''Function that find max value of an array and the index attached
+        Arg:
+            @param array : an array
+        
+        Returns max value and it index
+    '''
+    max=0
+    index=0
+    for i in range(len(array)):
+        if max < array[i]:
+            max=array[i]
+            index=i
+
+    return max, index
+
+array_max_one, index = max_value(array)
+print('The max number of \'array\' is {0} at index {1}'.format(array_max_one, index))
+
+array_max_two = max(array)
+print('The max number of \'array\' is {0} (using existing method)'.format(array_max_two))
