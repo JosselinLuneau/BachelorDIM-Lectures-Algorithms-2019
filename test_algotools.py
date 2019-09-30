ce4fc0033ac8513e2f0b4e555e14d7ba59a8b29c
Joss <josselinluneau@gmail.com>
2019-09-30 10:06:58 +0200

    Init TU in project

--- test_algotools.py
+++ test_algotools.py
@@ -7,12 +7,10 @@ Created on Sun Sep 29 21:44:54 2019
 """
 
 import pytest
+import S1_aglotools as algo
 
-def inc_(x):
-    return x+1
-
-def test_inc():
-    assert inc(3)==5
+def test_averrage_above_zero():
+        assert algo.average_above_zero(algo.array) == 6.125
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
