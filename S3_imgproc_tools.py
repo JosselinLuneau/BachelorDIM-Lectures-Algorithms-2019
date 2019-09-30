59ed2765a2a83507fa9c9ef424ee8c450fd37141
Joss <josselinluneau@gmail.com>
2019-09-30 14:53:13 +0200

    Session 3 - Exercice 1 : Invert color manually

--- S3_imgproc_tools.py
+++ S3_imgproc_tools.py
@@ -10,7 +10,22 @@ img_bgr=cv2.imread(img_name, 1) # load an image in Blue Green Red
 print("Gray levels image shape = "+str(img_gray.shape))
 print("BGR image shape = "+str(img_bgr.shape))
 
+
+def invert_colors_manual_slow(input_img):
+    for row in range(input_img.shape[0]):
+        for col in range(input_img.shape[1]):
+            for channel in range(input_img.shape[2]):
+                input_img[row, col, channel] = 255 - input_img[row, col, channel]
+    
+    return input_img
+
+def invert_colors_manual_fast(input_img):
+    return 255-input_img
+
+
 #display the loaded images
-cv2.imshow("Gray levels image", img_gray)
-cv2.imshow("BGR image", img_bgr)
+# cv2.imshow("Gray levels image", img_gray)
+# cv2.imshow("BGR image", img_bgr)
+# cv2.imshow("Inversed image (slow)", invert_colors_manual(img_bgr))
+cv2.imshow("Inversed image (fast)", invert_colors_manual_fast(img_bgr))
 cv2.waitKey()
